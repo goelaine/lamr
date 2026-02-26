76a8f62da040b9739568d3a1e4a521d87bf81946
goelaine <elainego@andrew.cmu.edu>
2026-02-26 00:45:25 -0500

    1a and 1b

--- homework/hw5/Numbermind.lean
+++ homework/hw5/Numbermind.lean
@@ -65,8 +65,27 @@ def decodeSolution (puzzleLen : Nat) (τ : List Lit): Except String (List Nat) :
 This is what you need to write: given the length of the rows and a list of guesses,
 print a solution.
 -/
+def buildNumberMind (puzzleLen : Nat) (guesses : List NumberMindGuess) : CnfForm :=
+  Id.run do
+  let mut cnf : CnfForm := []
+  -- let mut allLits := []
+  for i in [0:puzzleLen] do
+    let lits : List Lit := (List.range 10).map (fun v => mkLit i v)
+    cnf := (exactlyK lits 1).append cnf
+    -- allLits := allLits.append lits
+  for g in guesses do
+    cnf := (exactlyK g.lits g.k).append cnf
+  return cnf
+  -- return (exactlyK allLits puzzleLen).append cnf
+
 def solveNumberMind (puzzleLen : Nat) (guesses : List NumberMindGuess) : IO Unit := do
-  sorry
+  let (_, result) ← callCadical <| buildNumberMind puzzleLen guesses
+  match result with
+    | SatResult.Unsat _ => IO.println "unsat."
+    | SatResult.Sat   τ => match (decodeSolution puzzleLen τ) with
+      | Except.ok s => IO.println s
+      | Except.error e => IO.println "error"
+
 
 -- This lets us write `ofGuess` instead of `NumberMindGuess.ofGuess`
 open NumberMindGuess
@@ -82,7 +101,7 @@ def easyGame : List NumberMindGuess := [
 ]
 
 /- ok: [3, 9, 5, 4, 2] -/
--- #eval solveNumberMind 5 easyGame
+#eval solveNumberMind 5 easyGame
 
 def toughGame : List NumberMindGuess := [
   ofGuess [5,6,1,6,1,8,5,6,5,0,5,1,8,2,9,3] 2,
@@ -110,4 +129,4 @@ def toughGame : List NumberMindGuess := [
 ]
 
 /- This is the one you need to solve! -/
--- #eval solveNumberMind 16 toughGame
+#eval solveNumberMind 16 toughGame
